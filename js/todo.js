64ed0449ace8563b4df28103a9affea92542bb67
Nathan Birch <birchn@byui.edu>
2024-06-03 14:06:27 -0600

    final code from class today

--- js/todo.js
+++ js/todo.js
@@ -5,10 +5,26 @@ const addTodo = (event) => {
       const todoList = document.getElementById('todoList');
       const todoItemElement = document.createElement('li');
       todoItemElement.innerText = todoItem;
+
+      // Create delete button with FontAwesome icon
+      const deleteButton = document.createElement('span');
+      deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+      deleteButton.classList.add('delete-btn');
+      deleteButton.onclick = function () {
+        deleteTodo(todoItemElement);
+      };
+
+      // Append delete button to the list item
+      todoItemElement.appendChild(deleteButton);
       todoList.appendChild(todoItemElement);
+
       document.getElementById('todoInput').value = '';
     }
   }
+};
+
+const deleteTodo = (element) => {
+  element.remove();
 };
 
 document.getElementById('todoInput').addEventListener('keypress', addTodo);
